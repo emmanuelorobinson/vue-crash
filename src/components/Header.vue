5c5780f1be50ad68e9817dbb487becbf74be7dae
emmanuelorobinson <manuelejeay@gmail.com>
2022-08-05 00:21:54 +0400

    inital commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default{
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: 'Vue.js'
+    }
+  },
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+
+}
+</style>
